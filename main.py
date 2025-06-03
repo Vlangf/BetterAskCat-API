5f2eaa77abc06b65b01dc76937f9c9543efee3a2
Vlangf <vlangf@gmail.com>
2025-06-03 20:28:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 # Include the API router
 app.include_router(batteraskcat_router)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8002)

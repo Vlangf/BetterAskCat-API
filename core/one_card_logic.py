f2d00bf7edc074f57e8fff937251e3f36b7489aa
Vlangf <vlangf@gmail.com>
2025-06-03 19:26:41 +0200

    Update card logic and docker configuration

--- core/one_card_logic.py
+++ core/one_card_logic.py
@@ -3,7 +3,9 @@ from main import cards
 
 
 async def get_random_card():
-    card_name = random.choice(list(cards.keys()))
+    card_types = list(cards.keys())
+    card_type = random.choice(card_types)
+    card_name = random.choice(list(cards[card_type].keys()))
     card_data = cards[card_name]
     return card_name, card_data
 
